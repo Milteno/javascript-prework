94d60894f3ace8d0b8baa7a6833e599121b22fdd
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-01-12 03:40:26 +0100

    Add content in rock,paper,scissors game in script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
-
+let computerMove = "kamień";
 let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
@@ -13,4 +13,21 @@ else if(playerInput == "2") {
 else if(playerInput == "3") {
     playerMove = "nożyce";
 }
+window.printMessage('Ruch komputera to: ' + computerMove);
 window.printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz!');
+}
+else if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+}
+else if (playerMove == 'nieznany ruch') {
+    printMessage("Nieznany ruch!");
+}
+else {
+    printMessage("Nieznany ruch!");
+}
