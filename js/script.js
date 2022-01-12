784162b9d253841b85d4a8b6834e9441799def84
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-01-12 18:53:27 +0100

    Add function in script.js 5.5

--- js/script.js
+++ js/script.js
@@ -1,33 +1,18 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let playerMove = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let argPlayerMove = getMoveName(playerMove);
+let argComputerMove = getMoveName(randomNumber);
 
-console.log('Gracz wpisał: ' + playerInput);
-let computerMove = "kamień";
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+    if(argMoveId == 2){
+        return 'papier';
+      }
+      if(argMoveId == 3){
+        return 'nożyce';
+      }
+    
 }
-else if(playerInput == "2") {
-    playerMove = "papier";
-}
-else if(playerInput == "3") {
-    playerMove = "nożyce";
-}
-window.printMessage('Ruch komputera to: ' + computerMove);
-window.printMessage('Twój ruch to: ' + playerMove);
-
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  }
-else if (computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Przegrywasz!');
-}
-else if (computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis!');
-}
-else if (playerMove == 'nieznany ruch') {
-    printMessage("Nieznany ruch!");
-}
-else {
-    printMessage("Nieznany ruch!");
-}
+console.log('moves:', argComputerMove, argPlayerMove);
