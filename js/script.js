d5d0017242409ae80855103c6ead3762ecd5bac1
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-01-12 02:09:11 +0100

    Add new content in script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == "2") {
+    playerMove = "papier";
+}
+else if(playerInput == "3") {
+    playerMove = "nożyce";
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-let playerMove = `papier`;
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Twój ruch to: ' + playerMove);
