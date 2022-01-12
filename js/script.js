3d31bb5918d4628a0e2bbcb5a7450e122b6fc8a4
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-01-12 01:44:28 +0100

    Add let and printMessage in script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,6 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
