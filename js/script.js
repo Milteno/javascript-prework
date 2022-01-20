dac974f5df0b524b7167d05c63c11b944596a31a
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-01-20 18:15:31 +0100

    Improve code in script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+{
 document.getElementById("play-rock").addEventListener("click", function() {
   playGame(1);
 });
@@ -9,37 +10,33 @@ document.getElementById("play-scissors").addEventListener("click", function() {
   playGame(3);
 });
 
-function playGame(argMoveId) {
+const playGame = function(argMoveId) {
   clearMessages();
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  let argPlayerMove = getMoveName(argMoveId);
-  let argComputerMove = getMoveName(randomNumber);
 
-  function getMoveName(argMoveId){
-      if(argMoveId == 1){
-        return 'kamień';
+  const getMoveName = function(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+    else if (argMoveId == 2){
+      return 'papier';
       }
-      else if (argMoveId == 2){
-        return 'papier';
-        }
-      else if(argMoveId == 3){
-          return 'nożyce';
-        }
-  }
+    else if(argMoveId == 3){
+        return 'nożyce';
+      }
+}
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  const argPlayerMove = getMoveName(argMoveId);
+  const argComputerMove = getMoveName(randomNumber);
+
   printMessage("Mój ruch: " + argPlayerMove);
   printMessage("Ruch komputera: " + argComputerMove);
-  function displayResult(argComputerMove, argPlayerMove){
+
+  const displayResult = function(argComputerMove, argPlayerMove){
     if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
       printMessage('Ty wygrywasz!');
     }
     if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
       printMessage('Przegrywasz!');
-    }
-    if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
-      printMessage('Remis!');
-    }
-    if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
-      printMessage('Remis!');  
     }
     if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
       printMessage('Ty wygrywasz!');  
@@ -50,13 +47,14 @@ function playGame(argMoveId) {
     if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
       printMessage('Przegrywasz!');
     }  
-    if( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
-      printMessage('Remis!');
-    }  
     if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
       printMessage('Ty wygrywasz!');
     }  
+    if( argComputerMove == argPlayerMove){
+      printMessage('Remis!');
+    }
   }
   displayResult(argComputerMove, argPlayerMove);
   //console.log('moves:', argComputerMove, argPlayerMove);
+}
 }
